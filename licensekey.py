3294cf132867e03d9cef3b364ecbcf43df535942
Ganesh S <159233079+sganesh-999@users.noreply.github.com>
2024-08-31 09:06:22 +0530

    Update licensekey.py

--- licensekey.py
+++ licensekey.py
@@ -1,21 +1,11 @@
-s = '2-4A0r7-4k' 
-# 24-A0R-74K
-k = 3
-def process(s,k):
-    #split
-     temp=list("".join(s.upper().split("-")))
-     #reverse
-     temp=temp[::-1]
-     print('temp',temp)
-     updated_str=list()
-     #combine k charaters form list and join into str
-     for i in range(0,len(temp),k):
-         updated_str.append("".join(temp[i:i+k][::-1]))
-     print('updated_str',updated_str)
-     #reverse
-     updated_str=updated_str[::-1]
-     print('updated_str',updated_str)
-     print("-".join(updated_str))
-         
-     
-process(s,k)
+def licenseKeyFormatting(s, k):
+    s = s.upper().replace('-', '')
+    size = len(s)
+    s = s[::-1]
+    res = []
+    for i in range(0, size, k):
+        res.append(s[i:i+k])
+    return '-'.join(res)[::-1]
+
+print(licenseKeyFormatting("2-4A0r7-4k", 3))
+
